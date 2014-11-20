86f69da3b617082faa4f71e800efc00235466074
Gleb Ostrowski <glebos@web.de>
2014-11-20 16:24:55 +0900

    Initial commit, no code changes by me so far.

---
+++ src/native/cmat.h
@@ -0,0 +1,322 @@
+//
+//  cmat.h
+//
+//  Created by Hirotaka Moriguchi
+//
+
+#ifndef CMAT_cmat_h
+#define CMAT_cmat_h
+
+#include "nn.h"
+#include "Eigen/Core"
+
+#include <vector>
+#include <algorithm>
+
+
+#define PI 3.14159265358979323846
+using namespace std;
+using namespace Eigen;
+
+inline bool scoreComp(pair<double, int> a, pair<double, int> b){
+  return a.first < b.first;
+}
+
+class CMATWEANN{
+ private:
+  inline double randn(double mu, double sigma) {
+    static bool deviateAvailable=false;
+    static float storedDeviate;
+    double dist, angle;
+    if (!deviateAvailable) {
+      dist=sqrt( -2.0 * log(double(rand()) / double(RAND_MAX)) );
+      angle=2.0 * PI * (double(rand()) / double(RAND_MAX));
+      storedDeviate=dist*cos(angle);
+      deviateAvailable=true;
+      return dist * sin(angle) * sigma + mu;
+    }
+    else {
+      deviateAvailable=false;
+      return storedDeviate*sigma + mu;
+    }
+  }
+    
+  inline void expandM(MatrixXd& m, int rdiff, int cdiff){
+    MatrixXd newM = MatrixXd::Zero(m.rows()+rdiff,m.cols()+cdiff);
+    newM.block(0,0,m.rows(),m.cols()) = m;
+    m = newM;
+  }
+  inline void expandMiC(MatrixXi& m, int rdiff, int cdiff, int value){
+    MatrixXi newM = MatrixXi::Constant(m.rows()+rdiff,m.cols()+cdiff,value);
+    newM.block(0,0,m.rows(),m.cols()) = m;
+    m = newM;
+  }
+  inline void expandVZ(VectorXd& v, int sdiff){
+    VectorXd newV = VectorXd::Zero(v.size()+sdiff);
+    newV.head(v.size()) = v;
+    v = newV;
+  }
+  inline void expandVC(VectorXd& v, int sdiff, double value){
+    VectorXd newV = VectorXd::Constant(v.size()+sdiff,value);
+    newV.head(v.size()) = v;
+    v = newV;
+  }
+
+  inline void initializeCMAParameters(){
+    lambda = max((int)(4+(int)(3*log(dim))),5);
+    mu = (int)(lambda/2);
+    cc = (4./(4.+(double)dim));
+        
+    ccov = 2./pow(dim+sqrt(2.),2);
+        
+    wmu = VectorXd(mu);
+    for(int i = 0; i < mu; i++){
+      wmu(i) = log((lambda+1)/2) - log(i+1);
+    }
+    wmu/=wmu.sum();
+    cw = wmu.sum()/wmu.norm();
+        
+    mueff = 1/pow(wmu.norm(),2);
+    mucov = mueff;
+        
+    csigma = (mueff+2)/(dim+mueff+3);
+
+    dsigma = 1+2*max(0.,sqrt((mueff-1.)/(dim+1.))-1.)+csigma;
+        
+    ccu = sqrt(cc*(2-cc));
+    csigmau = sqrt(csigma*(2-csigma));
+    chin = dim*0.5*(1-1/(4*dim)+1/(21*dim*dim));
+        
+    score = VectorXd(lambda);
+    rank = VectorXi(lambda);
+  }
+  inline void updateCMAParameters(int ddiff, int ndiff){
+    dim+=ddiff; numHid+=ndiff;
+    initializeCMAParameters();
+        
+    expandVZ(pc,ddiff);
+        
+    expandVZ(psigma,ddiff);
+        
+    expandM(C,ddiff,ddiff);
+        
+    C.bottomRightCorner(ddiff,ddiff) = VectorXd::Constant(ddiff,pow(sigmaInit/sigma,2)).asDiagonal();
+        
+    expandVZ(xbase,ddiff);
+
+    expandVZ(zbase,ddiff);
+        
+    expandM(X,ddiff,lambda-(int)X.cols());
+    expandM(Z,ddiff,lambda-(int)Z.cols());
+        
+    expandM(B,ddiff,ddiff);
+    expandM(D,ddiff,ddiff);
+        
+    expandVZ(xvec,ddiff);
+  }
+
+  int numIn;
+  int numOut;
+  int numHid;
+  double sigma;
+  double sigmaInit;
+  double sigmaMin;
+  double probNode;
+  double probEdge;
+  bool bFF;
+    
+  int nctr;
+  /* following parameters will vary upon augmentation */
+  int dim;
+  int lambda;
+  int mu;
+  double cc;
+  double ccov;
+  double csigma;
+  double dsigma;
+  double ccu;
+  double csigmau;
+  double chin;
+    
+  VectorXd wmu;
+  double cw;
+  double mueff;
+  double mucov;
+  VectorXd score;
+  VectorXi rank;
+    
+  VectorXd pc;
+  VectorXd psigma;
+  MatrixXd C;
+  VectorXd xbase;
+    
+  VectorXd zbase;
+    
+  MatrixXd X;
+  MatrixXd Z;
+    
+  MatrixXd B;
+  MatrixXd D;
+    
+  VectorXd xvec;
+    
+  MatrixXi connectionMatrix;
+    
+  VectorXd w;
+  NN* nn;
+  /* above parameters will vary upon augmentation */
+    
+ public:
+ CMATWEANN(int numIn, int numOut, int numHid, double sigma, double sigmaMin, double probNode, double probEdge, bool bFF):
+  numIn(numIn),
+    numOut(numOut),
+    numHid(numHid),
+    sigma(sigma),
+    sigmaInit(sigma),
+    sigmaMin(sigmaMin),
+    probNode(probNode),
+    probEdge(probEdge),
+    bFF(bFF)
+    {
+      connectionMatrix = MatrixXi::Constant(numIn+numOut+numHid,numOut+numHid,-1);
+
+      // setup connections from input to hidden-output nodes
+      nctr = 0;
+      for(int i = 0; i < numIn; i++){
+	for(int j = 0; j < numOut+numHid; j++){
+	  connectionMatrix(i,j) = nctr;
+	  nctr++;
+	}
+      }
+
+      // setup feed-forward connections
+      if(bFF){
+	for(int i = numOut; i < numOut+numHid; i++){
+	  for(int j = 0; j < numOut; j++){
+	    connectionMatrix(i+numIn,j) = nctr;
+	    nctr++;
+	  }
+	}
+      }else{
+	for(int i = 0; i < numOut+numHid; i++){
+	  for(int j = 0; j < numOut+numHid; j++){
+	    connectionMatrix(i+numIn,j) = nctr;
+	    nctr++;
+	  }
+	}
+      }
+        
+      dim = nctr;
+      initializeCMAParameters();
+        
+      pc = VectorXd::Zero(dim);
+      psigma = VectorXd::Zero(dim);
+      C = VectorXd::Constant(dim,1).asDiagonal();
+      xbase = VectorXd::Zero(dim);
+      zbase = VectorXd::Zero(dim);
+        
+      X = MatrixXd::Zero(dim,lambda);
+      Z = MatrixXd::Zero(dim,lambda);
+        
+      B = VectorXd::Constant(dim,1).asDiagonal();
+      D = VectorXd::Constant(dim,1).asDiagonal();
+        
+      xvec = VectorXd::Zero(dim);
+        
+      nn = new NN(numIn, numOut, numHid, connectionMatrix, xbase, bFF);
+    }
+    
+  inline int getPopSize(){return lambda;}
+  NN* getNN(int nnID){
+    nn->setweight(X.col(nnID));
+    nn->reset();
+    return nn;
+  }
+  inline void setScore(int nnID, double tmpScore){score[nnID] = tmpScore;}
+  inline void produceOffspring(){
+    for(int i = 0; i < lambda; i++){
+      // produce mutation base
+      for(int j = 0; j < dim; j++) Z(j,i) = randn(0.,1.);
+      // mutation (x += C*z)
+      X.col(i) = xbase + sigma*B*D*Z.col(i);
+    }
+  }
+
+  void proceedGen(){
+    // sort z & x
+    // 1. reveal rank
+    double bestscore = 0;
+    
+    vector<pair<double, int> > rankScore(lambda);
+    for(int i = 0; i < lambda; i++){
+      rankScore[i] = make_pair(score[i],i);
+    }
+    sort(rankScore.begin(), rankScore.end(), scoreComp);
+    for(int i = 0; i < lambda; i++) rank[i] = rankScore[i].second;
+    
+    // 2. weighted sum
+    xbase = VectorXd::Zero(dim);
+    zbase = VectorXd::Zero(dim);
+    for(int i = 0; i < mu; i++){
+      xbase += X.col(rank[i])*wmu(i);
+      zbase += Z.col(rank[i])*wmu(i);
+    }
+        
+    // update pc and c
+    pc = (1-cc)*pc + ccu*sqrt(mueff)*B*D*zbase;
+        
+    // rank-mu update
+    MatrixXd Zmu = MatrixXd::Zero(dim,dim);
+    
+    for(int i = 0; i < mu; i++){
+      Zmu += Z.col(rank[i])*(Z.col(rank[i]).transpose())*wmu(i);
+    }
+
+    MatrixXd BDt = ((B*D).transpose());
+    Zmu = B*D*Zmu*BDt;
+    // rank-mu update
+    C = (1-ccov)*C + ccov*((1/mucov)*pc*pc.transpose() + (1-1/mucov)*Zmu);
+        
+    // update psigma and sigma
+    psigma = (1-csigma)*psigma + csigmau*sqrt(mueff)*B*zbase;
+    sigma = sigma * exp(csigma*(psigma.norm()-chin)/(dsigma*chin));
+        
+    if(bestscore > score[rank[0]]) bestscore = score[rank[0]];
+    /* augment topology here if needed */
+    // update wmap
+    double rand_mut = (double)random()/(double)RAND_MAX;
+    if(rand_mut < probNode){
+      expandMiC(connectionMatrix,1,1,-1);
+      connectionMatrix(random()%(numIn+numOut+numHid),numOut+numHid) = nctr;
+      nctr++;
+      connectionMatrix(numIn+numOut+numHid,(random()%(numOut+numHid))) = nctr;
+      nctr++;
+      updateCMAParameters(2,1);
+      nn = new NN(numIn, numOut, numHid, connectionMatrix, xbase, bFF);
+    }else if(rand_mut < (probNode+probEdge) && connectionMatrix.minCoeff() < 0){
+      double rand_row, rand_col;
+      while(1){
+	rand_row = random()%(numIn+numOut+numHid);
+	rand_col = random()%(numOut+numHid);
+	if(connectionMatrix(rand_row,rand_col) == -1) {
+	  connectionMatrix(rand_row,rand_col) = nctr;
+	  nctr++;
+	  break;
+	}
+      }
+      updateCMAParameters(1,0);
+      nn = new NN(numIn, numOut, numHid, connectionMatrix, xbase, bFF);
+    }
+        
+    JacobiSVD<MatrixXd> svd(C, ComputeFullU | ComputeFullV);
+    B = svd.matrixU();
+    D = svd.singularValues().asDiagonal();
+    D = D.array().sqrt().matrix();
+            
+    // lower bound on variance
+    if(sigma*D(dim-1,dim-1) < sigmaMin*sigmaInit)
+      sigma = sigmaMin*sigmaInit/D(dim-1,dim-1);
+  }
+};
+
+#endif
---
+++ src/native/nn.h
@@ -0,0 +1,103 @@
+//
+//  nn.h
+//
+//  Created by Hirotaka Moriguchi
+//
+
+#ifndef CMAT_nn_h
+#define CMAT_nn_h
+
+#include <iostream>
+#include "Eigen/Core"
+#include "Eigen/SVD"
+
+using namespace std;
+using namespace Eigen;
+
+class NN{
+    int numIn;
+    int numOut;
+    int numHid;
+    MatrixXi connectionMatrix; // (in+rec) x (rec), initialized with -1's
+    VectorXd weight;
+    
+    bool bFF;
+
+    VectorXd node;
+    VectorXd delay;
+    VectorXd inputs;
+    
+    inline double sigmoid(double x){
+        return x/(1+abs(x));
+    }
+    
+public:
+    NN(int in, int out, int hid, MatrixXi map, VectorXd w, bool ff){
+        numIn = in; numOut = out; numHid = hid; connectionMatrix = map; weight = w; bFF = ff;
+        node = VectorXd::Zero(numOut + numHid);
+        delay = VectorXd::Zero(numOut + numHid);
+    }
+    
+    NN(NN* nn):
+    numIn(nn->numIn), numOut(nn->numOut), numHid(nn->numHid), connectionMatrix(nn->connectionMatrix), weight(nn->weight), bFF(nn->bFF), node(nn->node), delay(nn->delay), inputs(nn->inputs){}
+    
+    inline double inverseSigmoid(double x){
+        if(x < 0){
+            return x/(x+1.);
+        }else{
+            return -x/(x-1.);
+        }
+    }
+
+    inline double ff_activation_iter(int node_no){
+        double tmp_ret = 0;
+        for(int i = 0; i < numIn; i++){
+            if(connectionMatrix(i,node_no) != -1)
+                tmp_ret += weight(connectionMatrix(i,node_no)) * inputs(i);
+        }
+        for(int i = numOut; i < numOut + numHid; i++){
+            if(connectionMatrix(numIn+i,node_no) != -1){
+                tmp_ret += weight(connectionMatrix(numIn+i,node_no))*ff_activation_iter(i);
+            }
+        }
+        return sigmoid(tmp_ret);
+    }
+    
+    inline void activate(VectorXd input, VectorXd& output){
+        if(bFF){
+            // feed-forward
+            inputs = input;
+            for(int i = 0; i < output.size(); i++){
+                output(i) = ff_activation_iter(i);
+            }
+        }else{
+            for(int j = 0; j < numOut + numHid; j++){
+                node(j) = 0;
+                for(int i = 0; i < numIn; i++){
+                    if(connectionMatrix(i,j) != -1){
+                        node(j) += weight(connectionMatrix(i,j)) * input(i);
+                    }
+                }
+                for(int i = 0; i < numOut + numHid; i++){
+                    if(connectionMatrix(i+numIn,j) != -1){
+                        node(j) += weight(connectionMatrix(i+numIn,j)) * delay(i);
+                    }
+                }
+            }
+            for(int i = 0; i < numOut + numHid; i++){
+                node(i) = sigmoid(node(i));
+                delay(i) = node(i);
+            }
+            for(int i = 0; i < output.size(); i++){
+                output(i) = node(i);
+            }
+        }
+    }
+    
+    inline void setweight(VectorXd w){weight = w;}
+    inline void reset(){delay = VectorXd::Zero(delay.size());}
+    
+    VectorXd getweight(){return weight;}
+};
+
+#endif
